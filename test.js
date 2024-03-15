10719ae3e9cebfa328a97329ad784896f1520bc4
afsana56 <afsanasheuly34@gmail.com>
2024-03-15 11:50:21 +0600

    all file added

---
+++ test.js
@@ -0,0 +1,27 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("lists-container");
+
+
+function addTask(){
+
+    if(inputBox.value == ""){
+        alert("please add text.")
+    }else{
+
+  
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement('span')
+    span.innerHTML = '&#9932';
+    li.appendChild(span)
+    inputBox.value = "";
+    localStorage.setItem("data",listContainer.innerHTML)  }
+}
+listContainer.addEventListener("click",function(e){
+    if (e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+    localStorage.setItem("data",listContainer.innerHTML)
+})
+listContainer.innerHTML = localStorage.getItem("data");
